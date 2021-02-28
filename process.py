43afdb1cef83c8336f918df74ccfcb46579d8e61
Stephanie <stephogam@gmail.com>
2021-02-28 05:58:20 +0000

    Added comments explaining what I believe each line is doing

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt") # allows file to open in terminal
+log_file = open("um-server-01.txt") # opens specified file
 
 
-def sales_reports(log_file): # function declaration with name and parameters
-    for line in log_file:    # iterating through a list or array
-        line = line.rstrip() # function definition that holds the body of code
+def sales_reports(log_file): # function declaration w parameter
+    for line in log_file:    # iterating through a file
+        line = line.rstrip() 
         day = line[0:3]
         if day == "Mon":     # if day is equal to Monday
-            print(line)      # print the line
+            print(line)         # print the line
 
 
-sales_reports(log_file) # executing the defined function
+sales_reports(log_file) # function execution with one argument
